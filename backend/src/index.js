5969a5a3ed7e75e253fd0129114693e825ab3d57
miguelraad28 <miguelraad2020@gmail.com>
2023-01-14 15:11:15 -0300

    .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,6 @@ const app = require("./app.js")
 require("./database.js")
 async function runServer(){
     await app.listen(process.env.PORT)
-    console.log(`Servidor en puerto process.env.PORT`)
+    console.log(`Servidor en puerto ${process.env.PORT}`)
 }
 runServer();
