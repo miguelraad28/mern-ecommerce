ff93c5f413c3f814bfbbd519e9e58781ffd77f47
miguelraad28 <miguelraad2020@gmail.com>
2023-01-24 18:52:28 -0300

    config

--- frontend/src/pages/Account/MyPurchases/Purchase.jsx
+++ frontend/src/pages/Account/MyPurchases/Purchase.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Purchase = ({_id, products, shippingCost, paymentMethod, subTotal, createdAt}) => {
+const Purchase = ({ _id, products, shippingCost, paymentMethod, subTotal, createdAt }) => {
     let createdAtModified = createdAt.slice(0, 10)
     createdAtModified = createdAtModified.split("-")
     createdAtModified = createdAtModified.reverse()
@@ -11,13 +11,13 @@ const Purchase = ({_id, products, shippingCost, paymentMethod, subTotal, created
             <p>{_id}</p>
             <p>{createdAtModified}</p>
             <p>{subTotal}</p>
-            <div style={{border: "2px solid red"}}>
-                {products ? (products.map(product => {
+            <div style={{ border: "2px solid red" }}>
+                {products ? (products.map(product => (
                     <>
-                    <p>{product.name}</p>
-                    <p>{product.price}</p>
-                    </>
-                })) : <h1> cargando...</h1>}
+                        <p>{product.name}</p>
+                        <p>{product.unitPrice}</p>
+                    </>)
+                )) : <h1> cargando...</h1>}
             </div>
             <p>{shippingCost}</p>
             <p>{paymentMethod}</p>
